66d1be0ec33fabc4723685bd10ce2f5db9e5acd0
Skyler <dare.skyler@gmail.com>
2024-04-02 14:36:34 -0400

    added monotone chain code

--- monotone_chain.py
+++ monotone_chain.py
@@ -1,2 +1,25 @@
+from graham_scan import cross_product as c
+import matplotlib as mp
+import matplotlib.animation as mpa
+
 def monotone_chain(points):
-	print("Hello World!")
+
+	points = sorted(set(points))
+
+	# Build lower hull
+	lower = []
+	for p in points:
+		while len(lower) >= 2 and cross(lower[-2], lower[-1], p) <= 0:
+			lower.pop()
+		lower.append(p)
+
+	# Build upper hull
+	upper = []
+	for p in reversed(points):
+		while len(upper) >= 2 and cross(upper[-2], upper[-1], p) <= 0:
+			upper.pop()
+		upper.append(p)
+
+	# Return combined hull, ignore the last points in the list
+	# as they are duplicates of the starting points in the lists
+	return lower[:-1] + upper[:-1]
