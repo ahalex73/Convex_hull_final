519e2460ccf5988c1be4ffd78cd3fa04c5d0c3da
TylerHitchcock11 <112909941+TylerHitchcock11@users.noreply.github.com>
2024-04-12 07:40:02 -0400

    Update timing.py
    added more comments to code to further explain

--- timing.py
+++ timing.py
@@ -1,29 +1,41 @@
-import graham_scan
+import graham_scan    # Import required modules
 import jarvis_march
 import monotone_chain
 import time
 import random
 
+# Initialize empty lists to store x and y coordinates of points
 x = []
 y = []
+# Define different sizes of points to test
 size = [10, 50, 100, 500, 1000, 2000, 4000, 6000, 8000, 10000]
+# Loop through each size
 for n in size:
+    # Initialize an empty list to store points
     points = []
+    # Generate random points within the range [-1, 1] for x and y coordinates
     for p in range(n):
         x = random.uniform(-1, 1)
         y = random.uniform(-1, 1)
         new_point = [x, y]
         points.append(new_point)
-
+        
+    # Initialize total time counter
     total_time = 0
+    # Perform 100 iterations for each algorithm
     for t in range(100):
+        # Start measuring time
         start_time = time.perf_counter_ns()
+        # Call the graham_scan algorithm to find the convex hull and make a copy of points
         graham_scan.find_convex_hull(points.copy())
+        # End measuring time
         end_time = time.perf_counter_ns()
+        # Calculate the time taken for each iteration
         total_time += end_time - start_time
-
+    # Print the average time taken for each iteration of Graham Scan algorithm
     print("Graham Scan " + str(n) + " points: " + str(total_time/100) + " nanoseconds.")
 
+    # Repeat the same process for Jarvis March algorithm
     total_time = 0
     for t in range(100):
         start_time = time.perf_counter_ns()
@@ -33,6 +45,7 @@ for n in size:
 
     print("Jarvis March " + str(n) + " points: " + str(total_time/100) + " nanoseconds.")
 
+    # Repeat the same process for Monotone Chain algorithm
     total_time = 0
     for t in range(100):
         start_time = time.perf_counter_ns()
